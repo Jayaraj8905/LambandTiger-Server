f5a086b6d2017d954cb4ecf5b11a6a0f5370c5b9
Jayaraj <jayaraj8905@gmail.com>
2021-05-30 18:21:00 +0800

    Mail send feature for portfolio application

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 const hostname = '192.168.1.80';
 const port = 3000;
 
+const nodemailer = require("nodemailer");
+
 const app = require('express')();
 var bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/', (req, res) => {
 	res.send('Please Use /api Endpoints!');
+})
+
+app.post('/api/sendmail', (req, res) => {
+	console.log(req.body);
+	const from = req.body['email'];
+	const name = req.body['name'];
+	const subject = `PORTFOLIO MAIL: ${req.body['subject']}`;
+	const message = req.body['message'];
+	const text = `MailId: ${from}
+Name: ${name}
+Message: ${message}`;
+	sendMail(from, name, subject, text);
+	res.send('success');
 })
 
 
@@ -122,3 +158,35 @@ function getClientBySocket(ws) {
 function getClientByIds(userId, gameId) {
 		return socketClients.find((socket) => socket.gameId == gameId && socket.userId == userId);
 }
+
+
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendMail(from, name, subject, text) {
+
+	var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+		  user: 'jayaraj8905@gmail.com',
+		  pass: 'password'
+		}
+	});
+
+	var mailOptions = {
+		from,
+		to: 'jayaraj8905@gmail.com',
+		subject,
+		text
+	};
+
+	let info = await transporter.sendMail(mailOptions);
+
+	console.log("Message sent: %s", info.messageId);
+	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+	// Preview only available when sending through an Ethereal account
+	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+	// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+  
+//   sendMail().catch(console.error);
